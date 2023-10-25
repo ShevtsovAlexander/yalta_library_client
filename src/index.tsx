2e8ed3cee680dbb10b6b8a0e169a5a5d0b44f4d7
ShevtsovAlexander <alexander.shevtsovvv.com>
2023-10-25 16:27:17 +0300

    update client

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import 'antd/dist/antd.css';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ConfigProvider } from 'antd';
+import ru_Ru from 'antd/lib/locale/ru_RU';
+import { createStore } from 'redux';
+import { rootReducer } from './Redux';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+import { Provider } from 'react-redux';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+export const store = createStore(rootReducer, composeWithDevTools());
+
+const app = (
+  <Provider store={store}>
+    <ConfigProvider locale={ru_Ru}>
+      <App />
+    </ConfigProvider>
+  </Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(app, document.getElementById('root'));
